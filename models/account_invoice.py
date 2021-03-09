1e09910133b36de421d3a88f7d6a521e45285a33
Sudish Shrestha <sudishrestha@gmail.com>
2021-03-09 13:16:13 +0545

    Auto select journal in register payment according to the mapping

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -10,3 +10,23 @@ class account_invoice(models.Model):
     
     payment_type = fields.Selection([('insurance', 'INSURANCE'), ('cash', 'CASH')], default='cash', string="Payment Type", required="True", help="Payment type accepted for this invoice", states={'draft': [('readonly', False)]})    
 
+
+class account_invoice(models.Model):
+    _name = 'account.payment'
+    _inherit = 'account.payment'
+
+    @api.multi
+    @api.onchange('amount')
+    def onchange_sale_orders_add_claim(self):
+        """
+        Trigger the change of sale order to add claims for associated sale orders
+        """
+        _logger.info("onchange_sale_orders_add_claim") 
+        paymentType = self.invoice_ids.payment_type
+        wt = self.env['payment.journal.mapping']
+        id_needed = wt.search([('payment_type', '=', paymentType)]).journal_id
+        if id_needed:
+            for payment in self:                
+                payment.update({
+                    'journal_id': id_needed
+                })    
